d857a3c69dfcc1608ae6470eb39f250bdd9dbb46
Jolan Plaum <jolan.plaum@student.howest.be>
2024-06-14 12:42:24 +0200

    VKT - Development environment

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,18 +1,35 @@
-#include "vulkanbase/VulkanBase.h"
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <iostream>
 
 int main() {
-	// DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1 = 1
-	//DISABLE_LAYER_NV_OPTIMUS_1 = 1
-	//_putenv_s("DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1", "1");
-	//_putenv_s("DISABLE_LAYER_NV_OPTIMUS_1", "1");
-	VulkanBase app;
+    glfwInit();
 
-	try {
-		app.run();
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		return EXIT_FAILURE;
-	}
-	return EXIT_SUCCESS;
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
 }
