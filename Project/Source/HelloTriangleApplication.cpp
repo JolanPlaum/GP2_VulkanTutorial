ff808cbd482f64a2e04ce5c241a28479fb68a4da
Jolan Plaum <jolan.plaum@student.howest.be>
2024-08-06 12:19:54 +0200

    Add extra plane to test mesh implementation

--- Project/Source/HelloTriangleApplication.cpp
+++ Project/Source/HelloTriangleApplication.cpp
@@ -239,10 +239,10 @@ void HelloTriangleApplication::UpdateUniformBuffer(uint32_t currentImage)
 	UniformBufferObject ubo{};
 
 	// Model Matrix
-	ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	ubo.model = glm::mat4(1);
 
 	// View Matrix
-	ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	ubo.view = glm::lookAt(glm::vec3(0.0f, -3.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 
 	// Projection Matrix
 	ubo.proj = glm::perspective(glm::radians(45.0f), m_SwapChainExtent.width / (float)m_SwapChainExtent.height, 0.1f, 10.0f);
