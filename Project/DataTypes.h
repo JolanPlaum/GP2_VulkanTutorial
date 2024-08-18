9e484fc9e2e5926625065a93bc02bc6825877a20
Jolan Plaum <jolan.plaum@student.howest.be>
2024-08-18 20:28:13 +0200

    Create VertexPBR layout
    This is to be used with PBR shader

--- Project/DataTypes.h
+++ Project/DataTypes.h
@@ -96,6 +96,58 @@ template<> struct std::hash<Vertex3D>
     }
 };
 
+struct VertexPBR
+{
+	glm::vec3 pos{ 0.f, 0.f, 0.f };
+	glm::vec3 normal{ 0.f, 0.f, 0.f };
+	glm::vec3 tangent{ 0.f, 0.f, 0.f };
+	glm::vec2 texCoord{ 0.f, 0.f };
+
+    bool operator==(const VertexPBR&) const = default;
+
+    static constexpr VkVertexInputBindingDescription GetBindingDescription() {
+        VkVertexInputBindingDescription bindingDescription{};
+        bindingDescription.binding = 0; // Index of the binding in the array of bindings
+        bindingDescription.stride = sizeof(VertexPBR); // Specifies number of bytes between entries
+        bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX; // Move to next data entry after vertex/intance
+        return bindingDescription;
+    }
+    static constexpr std::array<VkVertexInputAttributeDescription, 4> GetAttributeDescriptions() {
+        std::array<VkVertexInputAttributeDescription, 4> attributeDescriptions{}; // One for each member variable
+
+        attributeDescriptions[0].binding = 0;
+        attributeDescriptions[0].location = 0;
+        attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[0].offset = offsetof(VertexPBR, pos);
+
+        attributeDescriptions[1].binding = 0;
+        attributeDescriptions[1].location = 1;
+        attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[1].offset = offsetof(VertexPBR, normal);
+
+        attributeDescriptions[2].binding = 0;
+        attributeDescriptions[2].location = 2;
+        attributeDescriptions[2].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[2].offset = offsetof(VertexPBR, tangent);
+
+        attributeDescriptions[3].binding = 0;
+        attributeDescriptions[3].location = 3;
+        attributeDescriptions[3].format = VK_FORMAT_R32G32_SFLOAT;
+        attributeDescriptions[3].offset = offsetof(VertexPBR, texCoord);
+
+        return attributeDescriptions;
+    }
+};
+template<> struct std::hash<VertexPBR>
+{
+    size_t operator()(VertexPBR const& vertex) const {
+        return (( (( std::hash<glm::vec3>()(vertex.pos) ^
+            (std::hash<glm::vec3>()(vertex.normal) << 1)) >> 1) ^
+            (std::hash<glm::vec3>()(vertex.tangent) << 1)) >> 1) ^
+            (std::hash<glm::vec2>()(vertex.texCoord) << 1);
+    }
+};
+
 struct UniformBufferObject
 {
     alignas(16) glm::mat4 model;
